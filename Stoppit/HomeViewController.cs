14632c352e98b657240bbd71e723d2f667be6c84
Joe Lloyd <josephglloyd@gmail.com>
2015-07-19 17:03:27 +0100

    App now gets bossier when you say no, calling to a restful api with restsharp.

--- Stoppit/HomeViewController.cs
+++ Stoppit/HomeViewController.cs
@@ -1,5 +1,7 @@
 using System;
 using CoreGraphics;
+using Foundation;
+using RestSharp;
 using UIKit;
 
 namespace Stoppit
@@ -9,6 +11,7 @@ namespace Stoppit
 		private UIButton _button;
 
 		private readonly nfloat _xPadding = 10f;
+		private UILabel _label;
 
 		public override void ViewDidLoad()
 		{
@@ -16,13 +19,37 @@ namespace Stoppit
 
 			View.BackgroundColor = UIColor.White;
 
+			nfloat width = View.Bounds.Width - (_xPadding*2);
+
+			_label = new UILabel
+			{
+				Text = "Do as you're told",
+				Frame = new CGRect(_xPadding, 100, width, 31),
+				TextAlignment = UITextAlignment.Center,
+				AutoresizingMask = UIViewAutoresizing.FlexibleWidth
+			};
+
 			_button = UIButton.FromType(UIButtonType.RoundedRect);
 			_button.SetTitle("NO", UIControlState.Normal);
-			_button.Frame = new CGRect(_xPadding, 100, View.Bounds.Width - (_xPadding * 2), 44);
+			_button.Frame = new CGRect(_xPadding, 150, width, 44);
 			_button.BackgroundColor = UIColor.White;
 			_button.AutoresizingMask = UIViewAutoresizing.FlexibleWidth;
+			_button.TouchUpInside += (s, e) => { GetBossier(); };
 
+			View.AddSubview(_label);
 			View.AddSubview(_button);
+		}
+
+		private void GetBossier()
+		{
+			var client = new RestClient("http://doasyouretold.apphb.com/");
+			var request = new RestRequest("", Method.GET)
+			{
+				RequestFormat = DataFormat.Json
+			};
+
+			var response = client.Execute(request);
+			_label.Text = response.Content;
 		}
 	}
 }
